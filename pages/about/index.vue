4b936d1513151fddcc49eda3220b386d909157d6
kbaba1001 <kbaba1001@users.noreply.github.com>
2020-03-08 21:59:42 +0900

    new pages

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about-page">
+    <h1>This is my absolutely fantasitic "Amout Me" page!</h1>
+    <p>Lorem ipsum, ...</p>
+    <p>Lorem ipsum, ...</p>
+    <p>Lorem ipsum, ...</p>
+    <p>Lorem ipsum, ...</p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="sigle-post-page">
+    <section class="post">
+      <h1 class="post-title">Title of the Post</h1>
+      <div class="post-details">
+        <div class="post-detail">Last updated on XXX</div>
+        <div class="post-detail">Written by NAME</div>
+      </div>
+      <p>Content of the post</p>
+    </section>
+    <section class="post-feedback">
+      <p class="post-content">
+        Let me know what you think about this post, send a mail to
+        <a href="#">xxx@example.com</a>
+      </p>
+    </section>
+  </div>
+</template>
+
+<style scoped></style>
