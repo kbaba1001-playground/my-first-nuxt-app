467b251a852d17931eb9941b370b7b1bab87b454
kbaba1001 <kbaba1001@users.noreply.github.com>
2020-03-09 08:42:35 +0900

    index.vue and style

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,59 @@
 <template>
-  <div class="container"></div>
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech news!</h1>
+    </section>
+    <section class="featured-posts">
+      <nuxt-link :to="'/posts/' + 1">
+        <article class="poost-preview">
+          <div class="post-thumbnail"></div>
+          <div class="post-content">
+            <h1>Post Title</h1>
+            <p>Preview Text</p>
+          </div>
+        </article>
+      </nuxt-link>
+      <nuxt-link :to="'/posts/' + 2">
+        <article class="poost-preview">
+          <div class="post-thumbnail"></div>
+          <div class="post-content">
+            <h1>Post Title 2</h1>
+            <p>Preview Text 2</p>
+          </div>
+        </article>
+      </nuxt-link>
+    </section>
+  </div>
+
 </template>
 
 <script></script>
 
-<style></style>
+<style scoped>
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+</style>
