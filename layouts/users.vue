6a1cc7426dedd5aab45c37d39e6e14b76603c492
kbaba1001 <kbaba1001@users.noreply.github.com>
2020-03-08 12:22:29 +0900

    Header component

--- layouts/users.vue
+++ layouts/users.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
+    <Header />
     <header>
       USERS SECTION
     </header>
     <nuxt />
   </div>
 </template>
+
+<script>
+import Header from "@/components/Header";
+
+export default {
+  components: {
+    Header
+  }
+};
+</script>
 
 <style></style>
